50e084bf4e6d1521b68537e2a1813e4a57c35b55
SBolinha <157318106+SBolinha@users.noreply.github.com>
2024-01-24 23:44:15 +0100

    Update ECUsim.py

--- ECUSim/ECUsim.py
+++ ECUSim/ECUsim.py
@@ -1,1 +1,16 @@
+PID_Value_Map[PIDMemSize]
+MCP_CAN CAN(10) # CAN CS: pin 10
+
+setup():
+  Serial.begin(115200);
+  print("------------------- ECUSim setup start ---------------------------")
+  initializePIDValueMap()
+  initializeCAN()
+
+void loop():
+  if (Serial.available() >= SERIAL_MSG_LENGTH)
+    parsePIDUPdateMessage()
+  if (CAN.checkReceive() == CAN_MSGAVAIL)
+    handleCANMessage()
+
 
