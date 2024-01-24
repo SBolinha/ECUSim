31d7a41dbcdb5ce35db298fe240711ffe574ba7e
SBolinha <157318106+SBolinha@users.noreply.github.com>
2024-01-24 23:32:05 +0100

    Update ECUSim.h converted to Python

--- ECUSim/ECUSim.h
+++ ECUSim/ECUSim.h
@@ -1,33 +1,20 @@
-#ifndef ECUSIM_H
-#define ECUSIM_H
 
-#include "mcp_can.h"
-#include "mcp_can_dfs.h"
-#include <SPI.h>
-#include <avr/pgmspace.h>
-#include "PIDMap_Definition.h"
-#include <iso-tp.h>
 
-// ECU (this controller) CAN ID
-constexpr unsigned long ECU_CAN_ID = 0x7E0;
-constexpr unsigned long ECU_CAN_RESPONSE_ID = ECU_CAN_ID + 0x008;
+# ECU (this controller) CAN ID
+ECU_CAN_ID = 0x7E0;
+ECU_CAN_RESPONSE_ID = ECU_CAN_ID + 0x008;
 
-// ECU response(reply wait in ms)
-constexpr int ECU_WAIT = 0; 
+# ECU response(reply wait in ms)
+ECU_WAIT = 0; 
 
-// Debug message serial out switch
-constexpr bool PIDSET_DEBUG = false;
-constexpr bool PIDSET_ERROR = true;
-constexpr bool CANMSG_DEBUG = false;
-constexpr bool CANMSG_TIME_MEAS = false;
-constexpr bool CANMSG_FREERAM_MEAS = false;
-constexpr bool CANMSG_ERROR = true;
-constexpr bool CANMSG_FATAL = true;
+# Debug message serial out switch
+PIDSET_DEBUG = False;
+PIDSET_ERROR = True;
+CANMSG_DEBUG = False;
+CANMSG_TIME_MEAS = False;
+CANMSG_FREERAM_MEAS = False;
+CANMSG_ERROR = True;
+CANMSG_FATAL = True;
 
-extern byte PID_Value_Map[];
-extern MCP_CAN CAN;
-
-constexpr int SERIAL_MSG_LENGTH = 11;
-constexpr int CAN_PAYLOAD_LENGTH = 8;
-
-#endif
+SERIAL_MSG_LENGTH = 11;
+CAN_PAYLOAD_LENGTH = 8;
